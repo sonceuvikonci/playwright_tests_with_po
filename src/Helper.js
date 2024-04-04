2b31c40b9d8af2ce264dff8c5ae8e7a00718099f
martavion cordale <11692483-martavion@users.noreply.gitlab.com>
2024-04-04 12:40:49 +0300

    fix getRandomUniqueNumbersArray

--- src/Helper.js
+++ src/Helper.js
@@ -1,7 +1,7 @@
 export function getRandomUniqueNumbersArray(max) {
     const randomNumArr = [];
     while (randomNumArr.length < max) {
-        const random = Math.floor(Math.random() * 5);
+        const random = Math.floor(Math.random() * max);
         if (randomNumArr.indexOf(random) === -1) {
             randomNumArr.push(random);
         }
