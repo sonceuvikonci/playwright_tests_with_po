5eccfdcfa1a7a8641c61592c82a2928b2ac1abc1
martavion cordale <11692483-martavion@users.noreply.gitlab.com>
2024-04-04 12:40:17 +0300

    remove async foreach

--- src/globalPrototype.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-extend-native */
-Array.prototype.forEachAsync = async function (fn) {
-    for (let t of this) { await fn(t) }
-};
-
-Array.prototype.forEachAsyncParallel = async function (fn) {
-    await Promise.all(this.map(fn));
-};
